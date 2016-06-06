da253380103f8ee6dd4ace3f95297ee37f14d02a
Gabriellenhart <augustolenhart@gmail.com>
2016-06-05 23:54:15 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: 05/06/2016
+ *  4.   Agenda   telefônica   –   implementar   uma   agenda   telefônica   que   deve
+ * armazenar o nome, telefone da pessoa. Deve ser permitido inserir, remover
+ * e consultar as pessoas cadastradas;
+ *
+ *
+ *      Author: dell
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main(){
+
+		int
+	}
